a3b8f9188393954615c8e8327cc6b06068b52473
Giles <giles@Fuchsia.local>
2018-01-14 20:39:40 -0500

    run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -3,6 +3,10 @@ base=$HOME/workspace/StoryDoc
 cd $base
 echo Python get rm_release
 python3 python/getxml.py -t rm_release -q number=$release -o data/$release.release.xml
+if [ $? != 0 ]
+then
+  exit
+fi
 echo Python get rm_story
 python3 python/getxml.py -t rm_story -q active=true^release.number=$release -o data/$release.stories.xml
 echo XSLT step 1
